f64cd6ef609d4f94970124a65d5894bb364082b6
Guilherme <neveslourencog@gmail.com>
2025-11-28 20:58:10 -0300

    Removendo arquivos desnecessários, mantendo apenas DI.html

--- GitClone/heroi.py
+++
@@ -1,9 +0,0 @@
-from personagem import Personagem
-
-class Heroi(Personagem):
-    def __init__(self, nome, nivel, poder):
-        super().__init__(nome, nivel)
-        self.poder = poder
-
-    def __str__(self):
-        return f"Herói: {self.nome}, Nível: {self.nivel}, Poder: {self.poder}"
