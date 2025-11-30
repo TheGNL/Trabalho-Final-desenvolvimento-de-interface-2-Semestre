e779968a350e0ab0cb477529db7e6b4ba7f1d7e7
Guilherme Neves Lourenço <neveslourencog@gmail.com>
2025-11-29 22:56:04 -0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -5,8 +5,6 @@ import google.generativeai as genai
 app = Flask(__name__)
 CORS(app) 
 
-# --- CONFIGURAÇÃO ---
-# Lembre-se: Se for subir no GitHub, use variáveis de ambiente ou apague a chave antes!
 minha_chave = "Pegar chave no https://aistudio.google.com/api-keys" 
 genai.configure(api_key=minha_chave)
 
@@ -21,7 +19,6 @@ def chat():
         return jsonify({"erro": "Nenhuma mensagem enviada"}), 400
 
     try:
-        # MUDANÇA AQUI: Nova personalidade para o site de notícias
         prompt_sistema = (
             "Você é o 'Nexus AI', um assistente virtual especialista em videogames, "
             "e-sports (principalmente League of Legends) e tecnologia. "
@@ -33,7 +30,6 @@ def chat():
         return jsonify({"resposta": response.text})
     
     except Exception as e:
-        # --- ADICIONE ESTA LINHA ABAIXO PARA VER O ERRO NO TERMINAL ---
         print(f"ERRO DETALHADO: {e}") 
         # --------------------------------------------------------------
         return jsonify({"erro": str(e)}), 500
