c78e656db31a1a33a917b370f0f8f96196f206ca
Guilherme Neves Lourenço <neveslourencog@gmail.com>
2025-12-13 21:06:54 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from personagem import Personagem
-from heroi import Heroi
-
-if __name__ == "__main__":
-    personagem_comum = Personagem("Luffy", 100)
-    heroi_especial = Heroi("Goku", 999, "Kamehameha")
-
-    print(personagem_comum)
-    print(heroi_especial)
