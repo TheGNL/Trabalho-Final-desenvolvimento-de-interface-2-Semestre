bffdf5d0abddffd9e68e8dc192584a16a4fecb9e
Guilherme Neves Lourenço <neveslourencog@gmail.com>
2025-12-13 21:07:02 -0300

    Delete personagem.py

--- personagem.py
+++
@@ -1,7 +0,0 @@
-class Personagem:
-    def __init__(self, nome, nivel):
-        self.nome = nome
-        self.nivel = nivel
-
-    def __str__(self):
-        return f"Personagem: {self.nome}, Nível: {self.nivel}"
